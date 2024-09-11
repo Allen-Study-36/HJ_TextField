ae3e5e3abf4c910408852a0af74431344d3d9d15
hyejeong <dev.imhj7@gmail.com>
2024-09-11 15:12:34 +0900

    feat: textfield

--- TextField/TextField/ViewController.swift
+++ TextField/TextField/ViewController.swift
@@ -8,12 +8,76 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureTextField()
+        
+        textfield.delegate = self
+        textfield.becomeFirstResponder()
     }
-
-
+    
+    func configureTextField() {
+        textfield.placeholder = "E-mail Address"
+        textfield.borderStyle = .roundedRect
+        textfield.clearButtonMode = .always
+        textfield.keyboardType = .emailAddress
+        textfield.returnKeyType = .done
+    }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        
+        textfield.placeholder = "E-mail Address"
+        textField.clearsOnBeginEditing = true
+        
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        // 백스페이스 허용
+        if let char = string.cString(using: String.Encoding.utf8) {
+            let isBackSpace = strcmp(char, "\\b")
+            if isBackSpace == -92 {
+                return true
+            }
+        }
+        
+        // 문자만 허용
+        if string.allSatisfy({$0.isNumber}) {
+            return false
+        }
+        
+        // 10글자 이하 허용
+        guard let text = textField.text else { return true }
+        let newLength = text.count + string.count
+        return newLength <= 10
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textfield.resignFirstResponder()
+        
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        guard let text = textField.text else { return true }
+        
+        if text.isEmpty {
+            textField.placeholder = "Required"
+            return false
+        }
+        
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
